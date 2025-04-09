91f3e02d8e4b8a36368ca64e71cb0af09aa9d716
Safet <safetterziev8@gmail.com>
2025-04-09 20:18:29 +0300

    Refactor database connection to remove commented-out code for improved clarity

--- app.js
+++ app.js
@@ -43,14 +43,6 @@ app.use(bodyParser.json());
 
 const connection = mysql.createConnection(process.env.MYSQL_URL);
 
-// const connection = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   port: process.env.DB_PORT
-// });
-
 module.exports = connection.promise();
 
 connection.connect(err => {
